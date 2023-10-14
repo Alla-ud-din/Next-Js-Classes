46f7cab8c6d3b831f2c366cce56638fd7d749da5
Alla-ud-din <allauddin.aliahmad9@gmail.com>
2023-10-14 15:26:54 +0500

    updating files

--- class-01/src/app/product/dell/page.tsx
+++ class-01/src/app/product/dell/page.tsx
@@ -1,5 +1,10 @@
+import Link from "next/link"
 export default function page() {
     return (
-      <div>Dell laptop</div>
+      <div><h1>Dell laptop</h1>
+        <Link href = "./hp">Hp</Link>
+        <Link href = "./macbook">Macbook</Link>
+        <Link href = "./">Home</Link>
+      </div>
     )
   }
