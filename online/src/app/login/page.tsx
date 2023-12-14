5bd95acd563b33800cd884cd4f702d85ad976773
Alla-ud-din <allauddin.aliahmad9@gmail.com>
2023-12-14 16:42:53 +0500

    updating files

--- online/src/app/login/page.tsx
+++ online/src/app/login/page.tsx
@@ -1,8 +1,23 @@
+"use client"
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
-export default function Login() {
-  return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
-  )
+const Login = () => {
+    const router = useRouter();
+    const navigate = (page: string) => {
+        router.push("/login/" + page)
+    }
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <Link href="/" >Go to Home Page</Link>
+            <br /> <br />
+            <button onClick={() => navigate("loginstudent")} >Go to Student Login Page</button>
+            <br /> <br />
+            <button onClick={() => navigate("loginteacher")} >Go to Teacher Login Page</button>
+
+        </div>
+    )
 }
+
+export default Login;
--- online/src/app/page.tsx
+++ online/src/app/page.tsx
@@ -1,10 +1,24 @@
-import Image from 'next/image'
+"use client"
+import Link from 'next/link'
 import styles from './page.module.css'
+import {useRouter} from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter();
+  const navigate=(name: string)=>{
+    router.push(name)
+  }
   return (
-    <main className={styles.main}>
-      <h1>Basic Routing | Make New Page</h1>
+    <main>
+     <h1>Nested Routing</h1> 
+     <Link href="/login" >Go to Login Page</Link>
+     <br />
+     <br />
+     <Link href="/about" >Go to About Page</Link>
+     <br />
+     <br />
+     <button onClick={()=>navigate("/login")} >Go to Login Page</button>
+     <button onClick={()=>navigate("/about")} >Go to About Page</button>
     </main>
   )
 }
