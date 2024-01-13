a1a0aadf28d227a02de0d952299cda392ab78a64
Alla-ud-din <allauddin.aliahmad9@gmail.com>
2024-01-13 10:29:11 +0500

    updating files

--- react/src/app/page.tsx
+++ react/src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import {useEffect, useState} from "react"
+import { useEffect, useState } from "react"
 
 // class page extends React.Component{
 //   // count = 0
@@ -19,7 +19,7 @@ import {useEffect, useState} from "react"
 //   }
 //   increment(){
 //     // this.count = this.count + 1;
-   
+
 //     this.setState({count:this.state.count +1})
 //      console.log(this.state.count);
 //   }
@@ -34,6 +34,7 @@ import {useEffect, useState} from "react"
 //   }
 // }
 
+// <======================= useState =================>
 // function Page(){
 //   // let count = 0;
 //   const [count,setCount] = useState(0)
@@ -43,33 +44,86 @@ import {useEffect, useState} from "react"
 //     // count++;
 //     // console.log(count);
 //   }
+//   function decrement(){
+//     if(count >0){setCount(count-1)}
+//   }
 //   return (
-//     <div>Count: {count}
-//     <button onClick={()=>increment()}>Increment</button>
+//     <section className="flex flex-col">
+//     <div className="p-6">Count: {count}  </div>
+//     <div className="p-6">
+//     <button className="bg-blue-800" onClick={()=>increment()}>Increment</button>
+//     <button className="bg-blue-800" onClick={()=>decrement()}>Decrement</button>
 //     </div>
-
+//     </section>
 //   )
 // }
 
-function Page(){
- 
-  const [count1, setCount1] = useState(5);
-  const [count2, setCount2] = useState(5);
-  useEffect(()=>{
-    console.log("I am using Effect");
-  },[count2]);
-function increment1(){
-  setCount1(count1+1);
-}
-function increment2(){
-  setCount2(count2+1);
-}
-return (
-      <div>Count: {count1}
-      <button onClick={()=>increment1()}>Increment</button>
-      <button onClick={()=>increment2()}>Increment</button>
-      </div>
-  
-    )
-}
-export default Page;
+// <======================= useEffect =================>
+
+// Example 1
+// function Page() {
+
+//   const [count1, setCount1] = useState(0);
+//   const [count2, setCount2] = useState(0);
+//   useEffect(() => {
+//     console.log("count1: ", count1)
+//     console.log("count2: ", count2)
+//     return () => {
+//       console.log("I am using Effect");
+//     }
+//   }, [count2]);
+//   function increment1() {
+//     setCount1(count1 + 1);
+//   }
+//   // function increment2() {
+//   //   setCount2(count2 + 1);
+//   // }
+//   return (
+//     <section className="flex flex-col">
+//       <div className="p-6">Count: {count1}  & {count2}  </div>
+//       <div className="flex gap-4">
+//         <button className="bg-blue-400" onClick={() => increment1()}>Increment1</button>
+//         <button className="bg-blue-400" onClick={() => setCount2(count2 + 1)}>Increment2</button>
+//       </div>
+//     </section>
+//   )
+// }
+// export default Page;
+
+// Example 2
+
+
+// function Page() {
+//   const [count1, setCount1] = useState(5);
+//   const [count2, setCount2] = useState(0);
+//   const [todos, setTodos] = useState([]);
+
+//   useEffect( () => {
+//      getData();
+//   }, []);
+
+//   const getData = async () => {
+//     const data = await fetch("https://jsonplaceholder.typicode.com/todos");
+//     const response = await data.json();
+//     console.log(response);
+//     setTodos(response);
+//   };
+
+//   function increment2() {
+//     setCount2(count2 + 1);
+//     // count = count + 1;
+//     // console.log(count);
+//   }
+//   return (
+//     <div>
+//       Main {count1}
+//       {/* <button onClick={}>Increment</button> */}
+//       <br />
+//       count {count2}
+//       <button onClick={increment2}>Increment 2</button>
+//     </div>
+//   );
+// }
+
+// export default Page;
+
--- react/src/app/todo/page.tsx
+++ react/src/app/todo/page.tsx
@@ -1,44 +1,91 @@
 'use client'
-import React from 'react'
+import { useState } from "react";
 
-export default function page() {
-    const [input, setInput] = useState([]);
-    const [todos, setTodo] = useState([
-        {
-            id: 1,
-            title: "Todo1"
-        },
-        {
-            id: 2,
-            title: "Todo2"
-        },
-    ])
-    const addTodo = () => {
+// export default function page() {
+//     const [input, setInput] = useState([]);
+//     const [todos, setTodo] = useState([
+//         {
+//             id: 1,
+//             title: "Todo1"
+//         },
+//         {
+//             id: 2,
+//             title: "Todo2"
+//         },
+//     ])
+//     const addTodo = () => {
 
-    }
-    const removeTodo = () => {
+//     }
+//     const removeTodo = () => {
 
-    }
-    return (
+//     }
+//     return (
+//         <div>
+//             <div>
+//                 <div>
+//                     <input value="" type="text" placeholder='Add Todo' />
+//                     <button onClick={addTodo}>Add</button>
+//                 </div>
+//                 <div>
+//                     {todos.map((todo:any)=>(
+//                         <div className='flex bg-slate-100' key={todo.id}>
+//                             <h1></h1>
+//                         <button></button>
+//                         </div>
+//                     ))}
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+// function useState(arg0: { id: number; title: string }[]): [any, any] {
+//     throw new Error('Function not implemented.')
+// }
+
+
+interface todotype {
+    id: number,
+    title: string
+}
+
+export default function Page() {
+  const [input, setInput] = useState("");
+  const [todos, setTodo] = useState<todotype[]>([]);
+  const addTodo = () => {
+    setTodo([...todos, { id: Date.now(), title: input }]);
+    setInput("");
+  };
+  const removeTodo = (id: number) => {
+    const newTodos = todos.filter((todo: todotype) => todo.id !== id); // []
+    setTodo(newTodos);
+
+  };
+  return (
+    <div>
+      <div>
         <div>
-            <div>
-                <div>
-                    <input value="" type="text" placeholder='Add Todo' />
-                    <button onClick={addTodo}>Add</button>
-                </div>
-                <div>
-                    {todos.map((todo:any)=>(
-                        <div className='flex bg-slate-100' key={todo.id}>
-                            <h1></h1>
-                        <button></button>
-                        </div>
-                    ))}
-                </div>
-            </div>
+          <input
+            value={input}
+            type="text"
+            placeholder="Add Todo"
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button onClick={addTodo}>Add</button>
         </div>
-    )
+        <div className=" flex flex-col gap-2">
+          {todos.map((todo: todotype) => (
+            <div className=" flex bg-slate-100 gap-x-3" key={todo.id}>
+              <h1>{todo.title}</h1>
+              <button
+                onClick={() => removeTodo(todo.id)}
+                className=" border bg-slate-500"
+              >
+                Delete
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
-function useState(arg0: { id: number; title: string }[]): [any, any] {
-    throw new Error('Function not implemented.')
-}
-
